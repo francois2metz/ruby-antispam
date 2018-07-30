4a1aad832c114bb95da83b18b6dfc7ef40b1ed49
François de Metz <francois@2metz.fr>
2018-07-30 14:36:03 +0200

    Handle error message on ip info.

--- lib/cleantalk/ip_info_result.rb
+++ lib/cleantalk/ip_info_result.rb
@@ -1,3 +1,3 @@
 class Cleantalk::IpInfoResult < Cleantalk::Result
-  attr_reader :data
+  attr_reader :data, :error_message, :error_no
 end
--- spec/ip_info_result_spec.rb
+++ spec/ip_info_result_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Cleantalk::IpInfoResult do
-  let :init_values do
+  let(:normal_response) do
     {
       "data" => {
         "8.8.8.8" => {
@@ -12,10 +12,24 @@ describe Cleantalk::IpInfoResult do
     }
   end
 
-  it 'pass all result from JSON string' do
-    instance = described_class.new(JSON.fast_generate(init_values))
+  let(:error_response) do
+    {
+      "error_message" => "Calls limit exceeded.",
+      "error_no" => 10
+    }
+  end
 
-    init_values.each do |key, value|
+  it 'pass all result from JSON string' do
+    instance = described_class.new(JSON.fast_generate(normal_response))
+
+    normal_response.each do |key, value|
+      expect(instance.send(key)).to eql(value)
+    end
+  end
+
+  it 'handle errors' do
+    instance = described_class.new(JSON.fast_generate(error_response))
+    error_response.each do |key, value|
       expect(instance.send(key)).to eql(value)
     end
   end
